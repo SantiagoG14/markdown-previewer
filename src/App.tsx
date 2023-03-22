dff443c3cbfcb585be741864c9f0bb8e90b79965
Santiago Garcia <sagaan@outlook.com>
2023-03-21 20:30:57 -0700

    finished product

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react"
 import Markdown from "marked-react"
-import styled from "styled-components"
 
 function App() {
-  const [markdown, setMarkdown] = useState("")
+  const starterString = `# Hello\n## This is markdown btw\n[this is built with react](https://reactjs.org/)\n![react react](https://www.patterns.dev/img/reactjs/react-logo@3x.svg)\nto center a div use \` display: flex;\` \n\`\`\`\nif(thisIsTrue) {\n  callFunction()\n}\n\`\`\`
+\n> A wise man said that coding is not fun\n1.Display: flex;\n1. Display: none\n\n**bold text**`
+  const [markdown, setMarkdown] = useState(starterString)
 
   function handleInput(e: React.ChangeEvent<HTMLTextAreaElement>) {
     setMarkdown(e.target.value)
@@ -14,7 +15,7 @@ function App() {
       <h1 className=""> Hello tailwind</h1>
       <div className="marked">
         <textarea
-          id=""
+          id="editor"
           cols={100}
           rows={30}
           value={markdown}
@@ -22,8 +23,8 @@ function App() {
           className="input"
         ></textarea>
 
-        <div className="markdown">
-          <Markdown>{markdown}</Markdown>
+        <div className="markdown" id="preview">
+          <Markdown gfm={true}>{markdown}</Markdown>
         </div>
       </div>
     </div>
